3f7e3f9c0c0b3459864368a9b3cb4f8c0dff1889
maari <maari@LAPTOP-PH40GQON>
2021-04-18 20:27:13 +0200

    update

---
+++ actividad4/src/actividad4/princiapal.java
@@ -0,0 +1,15 @@
+package actividad4;
+
+public class princiapal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println ("Hola Mundo. Java Git");
+		System.out.println ("Hola Mundo. Z Java Git");
+	}
+	
+	
+
+}
