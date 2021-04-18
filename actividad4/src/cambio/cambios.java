6f0c39d6e3ab61f9bec3ca9d6f4c6e5b0f618ce8
maari <maari@LAPTOP-PH40GQON>
2021-04-18 21:11:18 +0200

    nueva_rama_cambios

---
+++ actividad4/src/cambio/cambios.java
@@ -0,0 +1,16 @@
+package cambio;
+
+public class cambios {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	    
+	    System.out.println("el cambio");
+	
+
+
+}
+}
+
